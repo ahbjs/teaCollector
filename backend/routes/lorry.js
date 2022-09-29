0b773326851f08028432df169fc5de0a7c6b06f8
ahbjs <34250287+ahbjs@users.noreply.github.com>
2022-09-29 21:32:25 +0530

    again

--- backend/routes/lorry.js
+++ backend/routes/lorry.js
@@ -6,15 +6,6 @@ const lorryModel = require("../model/lorry");
 /* GET home page. */
 router.get('/getLorry', async (req, res) => {
     
-    /*const lorry = new lorryModel({
-        Name : "asd",
-        Address : "sa",
-        NIC : "sad",
-        VehicleName : "asd",
-        VehicleNumber : "asd"
-    });
-    lorry.save()*/
-    
     lorryModel.find({}, async function (err, data) {
         if (err){
             console.log(err);
