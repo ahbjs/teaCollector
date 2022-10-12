2fbeead30a29cddd45238516af96da9a218c04ea
OKWijewardena <it20070458@my.sliit.lk>
2022-10-12 11:52:18 +0530

    update system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,36 @@
-
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-
-import AdminDashboard from './component/Admin/AdminDashboard';
-import RoadManagement from './component/Admin/RoadManagement';
-import AddRoad from './component/Admin/AddRoad';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Register from "./pages/register page/Register";
+import Login from "./pages/login page/Login";
+import PriceManagement from "./pages/priceManagement/priceManagement";
+import PriceList from "./pages/PriceList/PriceList";
+import UpdatePrice from "./pages/UpdatePrice page/UpdatePrice";
+import AdminDashboard from "./component/Admin/AdminDashboard";
+import AddRoad from "./component/Admin/AddRoad";
+import LorryManagement from "./pages/lorryManagement/lorryManagement";
 
 function App() {
   return (
-  <Router>
     <div>
-      <Routes>
-        <Route exact path="/" element={<AdminDashboard/>} />
-        <Route exact path="/RoadManagement" element={<RoadManagement/>} />
-        <Route exact path="/AddRoad" element={<AddRoad/>} />
-      </Routes>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/admin" component={AdminDashboard} />
+          <Route exact path="/AddRoad" component={AddRoad} />
+          <Route exact path="/PriceManagement" component={PriceManagement} />
+          <Route exact path="/PriceList" component={PriceList} />
+          <Route exact path="/LorryManagement" component={LorryManagement} />
+
+          <Route
+            exact
+            path="/PriceList/update/:sellerID"
+            component={UpdatePrice}
+          />
+        </Switch>
+      </Router>
     </div>
-  </Router>
   );
 }
 
--- frontend/src/component/Admin/AdminDashboard.js
+++ frontend/src/component/Admin/AdminDashboard.js
@@ -4,27 +4,11 @@ import 'font-awesome/css/font-awesome.min.css';
 import AdminSidebar from '../AdminSidebar';
 import '../../css/sidebar.css';
 import '../../css/admin.css';
-import axios from "axios";
 
 class AdminDashboard extends React.Component{
     constructor(props){
       super(props);
       this.state = {apiResponse:[]};
-      this.getLorry = this.getLorry.bind(this);
-    }
-
-    getLorry(data){
-        this.setState({
-            apiResponse : data
-        });
-        console.log(this.state.apiResponse);
-    }
-
-    componentDidMount(){
-        axios.get("http://localhost:8000/lorry/getLorry")
-        .then(data => this.getLorry(data.data))
-        .catch(error => console.log(error));
-        
     }
   
     render(){
@@ -46,17 +30,13 @@ class AdminDashboard extends React.Component{
                                     <th>Address</th>
                                     <th>NIC Number</th>
                                 </tr>
-                                {this.state.apiResponse.map(data => {
-                                    return(
-                                        <tr>
-                                            <td>{data.Name}</td>
-                                            <td>{data.VehicleName}</td>
-                                            <td>{data.VehicleNumber}</td>
-                                            <td>{data.Address}</td>
-                                            <td>{data.NIC}</td>
-                                        </tr>
-                                    )
-                                })}
+                                <tr>
+                                    <td>ahb</td>
+                                    <td>ahb</td>
+                                    <td>ahb</td>
+                                    <td>ahb</td>
+                                    <td>ahb</td>
+                                </tr>
                             </table>
                         </div>
 
