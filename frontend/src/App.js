226cddb1dd89b06df46df0275a00684bc8c83dc4
ahbjs <avishkahiranb@gmail.com>
2022-10-12 20:02:22 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
 
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
 import Register from "./pages/register page/Register";
 import Login from "./pages/login page/Login";
 import PriceManagement from "./pages/priceManagement/priceManagement";
 import PriceList from "./pages/PriceList/PriceList";
 import UpdatePrice from "./pages/UpdatePrice page/UpdatePrice";
-import AdminDashboard from "./component/Admin/AdminDashboard";
-import AddRoad from "./component/Admin/AddRoad";
 import LorryManagement from "./pages/lorryManagement/lorryManagement";
 
 import './App.css';
