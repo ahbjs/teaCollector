4ca1860ee731da34e6f897ee316a9bdf0ecfe5d4
IT20150266 <anuththaraabasnayaka@gmail.com>
2022-10-12 22:40:47 +0530

    Add frontend

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import "./Header.css"
+import logo_img from "../../../src/images/logo_img.png"
+import { BrowserRouter, Link } from "react-router-dom";
+
+export default function Header(){
+    return (
+      <div>
+        <img className="logo" src={logo_img} />
+        <h1 className="header">Tea Collector</h1>
+        <div className="bar"></div>
+          <Link to="/">
+            <p className="navBarText">Home</p>
+          </Link>
+          <Link to="/aboutus">
+            <p className="navBarText2">About Us</p>
+          </Link>
+          <Link to="/webbook">
+            <p className="navBarText3">Web Book</p>
+          </Link>
+          <Link to="/contactus">
+            <p className="navBarText4">Service Details</p>
+          </Link>
+      </div>
+    );
+}
